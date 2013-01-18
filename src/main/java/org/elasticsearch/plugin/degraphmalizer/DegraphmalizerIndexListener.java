bc11ca5de1b936bc3be8be50dc2ee821db13429c
Nils Breunese <nils@breun.nl>
2013-01-18 11:13:12 +0100

    DGM-23 Switched from pre to post listeners.

--- src/main/java/org/elasticsearch/plugin/degraphmalizer/DegraphmalizerIndexListener.java
+++ src/main/java/org/elasticsearch/plugin/degraphmalizer/DegraphmalizerIndexListener.java
@@ -21,38 +21,31 @@ public class DegraphmalizerIndexListener extends IndexingOperationListener
     }
 
     @Override
-    public Engine.Create preCreate(Engine.Create createOperation)
+    public void postCreate(Engine.Create createOperation)
     {
         final String type = createOperation.type();
         final String id = createOperation.id();
         final long version = createOperation.version();
 
         graphUpdater.add(GraphChange.update(index, type, id, version));
-
-        return createOperation;
     }
 
     @Override
-    public Engine.Index preIndex(Engine.Index indexOperation)
-    {
+    public void postIndex(Engine.Index indexOperation) {
         final String type = indexOperation.type();
         final String id = indexOperation.id();
         final long version = indexOperation.version();
 
         graphUpdater.add(GraphChange.update(index, type, id, version));
-
-        return indexOperation;
     }
 
     @Override
-    public Engine.Delete preDelete(Engine.Delete deleteOperation)
+    public void postDelete(Engine.Delete deleteOperation)
     {
         final String type = deleteOperation.type();
         final String id = deleteOperation.id();
         final long version = deleteOperation.version();
 
         graphUpdater.add(GraphChange.delete(index, type, id, version));
-
-        return deleteOperation;
     }
 }
