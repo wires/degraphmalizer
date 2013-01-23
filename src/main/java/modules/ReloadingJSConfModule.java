275362323ab7bb82aedafb2bf13d02dcdd582c68
Nils Breunese <N.Breunese@vpro.nl>
2013-01-23 15:22:24 +0000

    DGM-49 Real men use logging.

--- src/main/java/modules/ReloadingJSConfModule.java
+++ src/main/java/modules/ReloadingJSConfModule.java
@@ -9,11 +9,16 @@ import configuration.javascript.PollingConfigurationMonitor;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Non-reloading javascript configuration
  */
 public class ReloadingJSConfModule extends AbstractModule implements ConfigurationMonitor
 {
+    private final static Logger log = LoggerFactory.getLogger(ReloadingJSConfModule.class);
+
 	final String scriptFolder;
     final PollingConfigurationMonitor poller;
 
@@ -58,8 +63,7 @@ public class ReloadingJSConfModule extends AbstractModule implements Configurati
         // when the configurating changes, mark it for (lazy) reloading
         configuration = null;
 
-        // TODO fix logging
-        System.err.println("Configuration change detected for index '" + index + "'");
+        log.info("Configuration change detected for index {}", index);
     }
 
     private JavascriptConfiguration loadConf() throws IOException
