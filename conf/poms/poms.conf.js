8579e10969c88d819e03e2f5c8f16220839ef9c6
Ernst Bunders <E.Bunders@vpro.nl>
2013-01-23 14:58:54 +0000

    new extended version

--- conf/poms/poms.conf.js
+++ conf/poms/poms.conf.js
@@ -1,20 +1,53 @@
 ({
-	filter: (function(doc) {
-		return true;
-	}),
+    targetIndex: "koekje",
+    targetType: "pretzel",
 
-	extract: (function(doc, subgraph) {
-		if(doc.parent)
-			subgraph.add_edge_from("parent", doc.parent);
-	}),
+    filter: (function(doc) {
+        return true;
+    }),
+
+    // void addEdge(String label,
+    //          String index, String type, String id,
+    //          boolean inwards,
+    //          Map<String, Object> properties) throws IOException
+    // void setProperty(String key, Object value) throws IOException
+
+    extract: (function(doc, subgraph)
+    {
+        // for each episodeOf, add an edge (us) -- episodeOf --> urn
+        if (doc.episodeOf && doc.episodeOf.length)
+        {
+            doc.episodeOf.forEach(function(program){
+                // add edge with some properties
+                subgraph.addEdge("episodeOf", "poms", "doc", program['reference'], false,
+                    {
+                        position: program['position'],
+                        added: program['added']
+                    });
+            });
+        }
+
+
+    }),
 
     "walks":{
+        "backward":{
+            "direction": "OUT",
+            "properties": {
+                "nodes-pointing-to-us": {
+                    nested: true,
+                    reduce: function(doc_tree)
+                    {
+                        return doc_tree;
+                    }
+                }
+            }
+        },
         "forward":{
             "direction": "IN",
             "properties": {
-                "nodes-out": {
+                "nodes-we-are-pointing-to": {
                     nested: true,
-
                     reduce: function(doc_tree)
                     {
                         return doc_tree;
