425688dd4f7e52980e106626e7bab22efbaf67dd
Nils Breunese <N.Breunese@vpro.nl>
2013-01-23 15:25:41 +0000

    DGM-49 Accolades.

--- src/main/java/streaming/GraphStreamerHttpHandler.java
+++ src/main/java/streaming/GraphStreamerHttpHandler.java
@@ -14,15 +14,16 @@ import streaming.requestmapper.handlers.GlobalCommandStreamRequestHandler;
 
 import java.util.List;
 
-public final class GraphStreamerHttpHandler extends SimpleChannelHandler {
-
+public final class GraphStreamerHttpHandler extends SimpleChannelHandler
+{
     private static final Logger LOGGER = LoggerFactory.getLogger(GraphStreamerHttpHandler.class);
 
     private final HttpRequestMapper requestMapper;
     private final ChannelContext channelContext;
     private final StreamingGraph streamingGraph;
 
-    public GraphStreamerHttpHandler(HttpRequestMapper requestMapper, ChannelContext channelContext, StreamingGraph streamingGraph) {
+    public GraphStreamerHttpHandler(HttpRequestMapper requestMapper, ChannelContext channelContext, StreamingGraph streamingGraph)
+    {
         this.requestMapper = requestMapper;
         this.channelContext = channelContext;
         this.streamingGraph = streamingGraph;
@@ -55,34 +56,39 @@ public final class GraphStreamerHttpHandler extends SimpleChannelHandler {
         }
     }
 
-    private HttpResponse createErrorResponse(RequestHandlerException rhe) {
+    private HttpResponse createErrorResponse(RequestHandlerException rhe)
+    {
         HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, rhe.getStatus());
         response.setContent(ChannelBuffers.wrappedBuffer(rhe.getMessage().getBytes()));
         return response;
     }
 
     @Override
-    public void channelClosed(ChannelHandlerContext ctx, final ChannelStateEvent event) throws Exception {
+    public void channelClosed(ChannelHandlerContext ctx, final ChannelStateEvent event) throws Exception
+    {
         //TODO: somehow this stuff should be moved into the HttpRequestMapper.RequestHandler interface, so the handlers can 'clean up their own mess'.
         //now this channel closes, remove it from the context, no more writing to it.
         channelContext.removeChannel(event.getChannel());
 
         //we also have to check if a streaming.requestmapper.handlers.GlobalCommandStreamRequestHandler.ChannelpushingGraphCommandListener
         //has been registered for this channel, if so: remove that listener
-        List<GraphCommandListener> listener = streamingGraph.findListeners(new StreamingGraph.GraphCommandListenerFilter() {
+        List<GraphCommandListener> listener = streamingGraph.findListeners(new StreamingGraph.GraphCommandListenerFilter()
+        {
             @Override
-            public boolean matches(GraphCommandListener listener) {
-                if (GlobalCommandStreamRequestHandler.ChannelpushingGraphCommandListener.class.isAssignableFrom(listener.getClass())) {
+            public boolean matches(GraphCommandListener listener)
+            {
+                if (GlobalCommandStreamRequestHandler.ChannelpushingGraphCommandListener.class.isAssignableFrom(listener.getClass()))
+                {
                     GlobalCommandStreamRequestHandler.ChannelpushingGraphCommandListener channelPushingListener = (GlobalCommandStreamRequestHandler.ChannelpushingGraphCommandListener) listener;
-                    if (channelPushingListener.getChannel().equals(event.getChannel())) {
+                    if (channelPushingListener.getChannel().equals(event.getChannel()))
                         return true;
-                    }
                 }
                 return false;
             }
         });
 
-        if (listener.size() > 0) {
+        if (listener.size() > 0)
+        {
             //there shouldn't be more than one
             if(listener.size() > 1)
                 LOGGER.warn("There is more than one ChannelpushingGraphCommandListener registered with the StreamingGraph for channel " + event.getChannel());
@@ -93,7 +99,8 @@ public final class GraphStreamerHttpHandler extends SimpleChannelHandler {
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception
+    {
         //TODO handle this stuff.
     }
 }
