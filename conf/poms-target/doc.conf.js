e93453e7adfd664b9cbea2101a9d382cdc23a8d6
Jelle Herold <jelle@defekt.nl>
2013-01-28 13:35:41 +0000

    cleanup: fixed indentation

--- conf/poms-target/doc.conf.js
+++ conf/poms-target/doc.conf.js
@@ -14,71 +14,71 @@
 
     extract: (function(doc, subgraph)
     {
-//       subgraph.addEdge("testEdge", "poms", "poms", "boe", true, {});
+	// _.each(doc.episodeOf || [], function(p) { ... })
 
-        // _.each(doc.episodeOf || [], function(p) { ... })
+	// safely map a function over an array
 	var allOf = function(elt, fn) {
-	  if(elt && elt.length) {
-            elt.forEach(fn)
-	  }
+	    if (elt && elt.length) {
+		elt.forEach(fn)
+	    }
 	};
 
         // for each episodeOf, add an edge (us) -- episodeOf --> urn
 	allOf(doc.episodeOf, function(program) {
-                // add edge with some properties
-                subgraph.addEdge("episodeOf", "poms", "poms", program['reference'], false,
-                    {
-                        position: program['position'],
-                        added: program['added']
-                    });
-        });
+	    subgraph.addEdge("episodeOf", "poms", "poms", program['reference'], false, {});
+	});
 
-        allOf(doc.descendantOf, function(program) {
-            subgraph.addEdge("descendantOf", "poms", "poms", program['urnRef'], false, {});
-        });
+	allOf(doc.descendantOf, function(program) {
+	    subgraph.addEdge("descendantOf", "poms", "poms", program['urnRef'], false, {});
+	});
 
-		if (doc.mid) {
-			subgraph.addEdge("mid", "subtitles-source", "subtitle", doc.mid, true, {});
-		}
+	// link from 'mid' node (to which subtitles link)
+	if (doc.mid) {
+	    subgraph.addEdge("mid", "subtitles-source", "subtitle", doc.mid, true, {});
+	}
+
     }),
 
     "walks":{
-        "backward":{
-            "direction": "OUT",
-            "properties": {
-                "nodes-in": {
-                    nested: true,
-                    reduce: function(doc_tree)
-                    {
-                        return doc_tree;
-                    }
-                },
-                "teletekst-data": {
-                    nested: true,
-                    reduce: function(doc_tree)
-                    {
+
+	"backward":{
+	    "direction": "OUT",
+	    "properties": {
+
+		"nodes-in": {
+		    nested: true,
+		    reduce: function(doc_tree) {
+			    return doc_tree;
+		    }
+		},
+
+		"teletekst-data": {
+		    nested: true,
+		    reduce: function(doc_tree) {
+
 			var data = "";
-			(doc_tree._children || []).map ( function (c)  {
-				if (c.text != null) {
-					data += c.text + " ";
-				}
+			(doc_tree._children || []).map( function (c)  {
+			    if (c.text != null) {
+				data += c.text + " ";
+			    }
 			});
-                        return data;
-                    }
+
+			return data;
+		    }
 		}
-            }
-        },
-        "forward":{
-            "direction": "IN",
-            "properties": {
-                "nodes-out": {
-                    nested: true,
-                    reduce: function(doc_tree)
-                    {
-                        return doc_tree;
-                    }
-                }
-            }
-        }
+	    }
+	},
+
+	"forward":{
+	    "direction": "IN",
+	    "properties": {
+		"nodes-out": {
+		    nested: true,
+		    reduce: function(doc_tree) {
+			return doc_tree;
+		    }
+		}
+	    }
+	}
     }
 })
