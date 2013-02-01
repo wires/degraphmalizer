fa84cfcca5cf7c6116db85b77cd2eda1a7b208df
Jelle Herold <jelle@defekt.nl>
2013-02-01 15:18:31 +0000

    fix: confusing message was printed

--- src/main/java/graphs/ops/LoggingSubgraph.java
+++ src/main/java/graphs/ops/LoggingSubgraph.java
@@ -1,14 +1,12 @@
 package graphs.ops;
 
-import java.util.Map;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.tinkerpop.blueprints.Direction;
-
+import degraphmalizr.ID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import degraphmalizr.ID;
+import java.util.Map;
 
 public class LoggingSubgraph implements Subgraph
 {
@@ -25,7 +23,8 @@ public class LoggingSubgraph implements Subgraph
     public final void addEdge(String label, ID other, Direction direction, Map<String, JsonNode> properties)
     {
         final String d = direction == Direction.IN ? "to" : "from";
-        log.info(prefix + " edge " + d + " self to " + other + " with label " + label + " created");
+        final String e = direction == Direction.OUT ? "to " : "from ";
+        log.info(prefix + " edge " + d + " self " + e + other + " with label " + label + " created");
     }
 
     @Override
